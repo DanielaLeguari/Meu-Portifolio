120a17ffdc0cd8bc9d0ad0c1f8fb90adebc10c4f
Daniela Leguari <danielaleguari@outlook.com>
2023-02-08 17:40:55 -0300

    paginas de contato e obrigado

--- src/pages/portifolio/script.js
+++ src/pages/portifolio/script.js
@@ -1,19 +1,19 @@
 fetch('./projetos.json').then(response => {
-    return response.json();
+  return response.json();
 }).then(data => {
-    let html = '';
-    console.log(data);
-    data.forEach(elemento => {
-        html += `  <article id="card" class="card card-estilo card-largura p-0 m-4 col-12 col-md-3">
+  let html = '';
+  console.log(data);
+  data.forEach(elemento => {
+    html += `  <article id="card" class="card card-estilo card-largura p-0 m-4 col-12 col-md-3">
         <img  id="imagem" src="${elemento.imagem}"class="card-img-top card-posicao-imagem" alt="...">
         <div class="card-body">
           <h5 id="titulo" class="card-title">${elemento.titulo}</h5>
           <p id="subtitulo" class="card-text">${elemento.legenda}</p>
-          <a href="${elemento.link}"class="btn btn-dark me-md-2">Acessar projeto</a>
+          <a href="${elemento.link}"class="botao-cor-projetos">Acessar projeto</a>
         </div>
       </article>`
-    });
-    document.getElementById("cards-projetos").innerHTML = html;
+  });
+  document.getElementById("cards-projetos").innerHTML = html;
 }).catch(err => {
-    console.log(err);
+  console.log(err);
 });
