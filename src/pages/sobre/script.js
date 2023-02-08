876ea8e8e5a5a2f73d270f6b2d91e4a34b86a3eb
Daniela Leguari <danielaleguari@outlook.com>
2023-02-08 17:51:29 -0300

    ajustes css

--- src/pages/sobre/script.js
+++ src/pages/sobre/script.js
@@ -7,9 +7,9 @@ fetch('./cursos.json').then(response => {
         html +=
         `
     <article class="card card-largura  p-0 m-4 col-12 col-md-3" style="width: 18rem;">
-    <div class="card-body borda-card-cursos">
-        <h5 class="card-title">${elemento.titulo}</h5>
-        <p class="card-text">${elemento.instituicao}</p>
+    <div class="card-body borda-card-cursos fonte-cursos ">
+        <h5 class="card-title titulo-card-cursos">${elemento.titulo}</h5>
+        <p class="card-text text-secundary">${elemento.instituicao}</p>
     </div>
 </article>
         `
