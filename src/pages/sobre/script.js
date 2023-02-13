b0f6b75d6fdd67d202f58c7362bda67b086c1574
Daniela Leguari <danielaleguari@outlook.com>
2023-02-13 16:45:59 -0300

    estilos pagina inicial e sobre

--- src/pages/sobre/script.js
+++ src/pages/sobre/script.js
@@ -6,12 +6,12 @@ fetch('./cursos.json').then(response => {
     data.forEach(elemento =>
         html +=
         `
-    <article class="card card-largura  p-0 m-4 col-12 col-md-3" style="width: 18rem;">
-    <div class="card-body borda-card-cursos fonte-cursos ">
-        <h5 class="card-title titulo-card-cursos">${elemento.titulo}</h5>
-        <p class="card-text text-secundary">${elemento.instituicao}</p>
-    </div>
-</article>
+    <article class="card card-largura  p-2 m-4 col-12 col-md-3" style="width: 18rem;">
+        <div class="card-body fonte-cursos ">
+            <h5 class="card-title titulo-card-cursos">${elemento.titulo}</h5>
+            <p class="card-text p-0 text-secundary">${elemento.instituicao}</p>
+        </div>
+    </article>
         `
     );
     document.getElementById("cursos").innerHTML = html;
